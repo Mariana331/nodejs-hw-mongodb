14d9ce717182a42c591fa74777d83b04e3edcf83
Mariana_k <m.koval000@gmail.com>
2025-10-03 21:02:04 +0300

    fixed controllers

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -68,7 +68,7 @@ export const createContactController = async (req, res) => {
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const userId = req.user._id;
-  const contact = await updateContact(contactId, req.body, userId);
+  const contact = await updateContact(contactId, userId, req.body);
 
   if (!contact) {
     return next(createHttpError(404, 'Contact not found'));
