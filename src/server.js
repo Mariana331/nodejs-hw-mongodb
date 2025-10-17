d72dea35485eaa6b7e9b1e55ac21a5a8204607eb
Mariana_k <m.koval000@gmail.com>
2025-10-17 13:29:45 +0300

    add files

--- src/server.js
+++ src/server.js
@@ -7,12 +7,20 @@ import usersRouters from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import swaggerUI from 'swagger-ui-express';
+import * as fs from 'node:fs';
+import path from 'path';
+
+const SWAGGER_DOCUMENT = JSON.parse(
+  fs.readFileSync(path.join('docs', 'swagger.json')),
+);
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
   app.use(cors());
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCUMENT));
   app.use(express.json());
   app.use(cookieParser());
 
